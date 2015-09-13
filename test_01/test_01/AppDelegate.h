1440b94a3791c0113895f2ed6eddb11774546424
lijinghao <lijinghao@lijinghaodeMacBook-Pro.local>
2015-09-13 23:04:49 +0800

    test_01

---
+++ test_01/test_01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_01
+//
+//  Created by lijinghao on 15/9/13.
+//  Copyright (c) 2015年 lijinghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_01/test_01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_01
+//
+//  Created by lijinghao on 15/9/13.
+//  Copyright (c) 2015年 lijinghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
